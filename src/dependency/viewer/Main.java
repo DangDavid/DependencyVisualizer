6b596eab72cb82cb088573908e7b3de72e833ee5
David Dang <daviddang.92@gmail.com>
2013-10-28 10:16:46 -0700

    changed filefilter to searcher

--- src/dependency/viewer/Main.java
+++ src/dependency/viewer/Main.java
@@ -1,6 +1,6 @@
 package dependency.viewer;
 
-import dependency.viewer.parser.FileFilter;
+import dependency.viewer.parser.FileSearcher;
 import dependency.viewer.parser.ModuleData;
 import dependency.viewer.parser.XmlParser;
 
@@ -26,7 +26,7 @@ public class Main {
     }
 
     private static List<ModuleData> parseStep() {
-        FileFilter files = new FileFilter();
+        FileSearcher files = new FileSearcher();
         //List<String> filePaths = files.getFiles("F:\\Downloads\\pidgin-2.10.7.tar\\pidgin-2.10.7\\pidgin-2.10.7\\finch\\xml");
         List<String> filePaths = files.getFiles("/Users/taehyunkang/Downloads/pidgin-2.10.7/finch/xml");
 
