67f054f87083eec5724cd7ae6a08fe08835f17b9
Charles-Luo <Charles-Luo@Charles-PC>
2013-11-16 21:30:14 -0800

    Comment DependencyGraph.java

--- src/dependency/viewer/mapper/DependencyGraph.java
+++ src/dependency/viewer/mapper/DependencyGraph.java
@@ -8,160 +8,156 @@ import java.util.*;
  * Date: 26/10/13
  * Time: 1:29 PM
  * <p/>
- * This class should only be used to create a visualization on. To create and set up the data, use
- * the DependencyData class to add dependencies.
+ * This class makes abstract the structure of a DependencyData by making matrix out of it, and 
+ * it also groups modules to form clusters by running certain algorithms.
  */
 public class DependencyGraph {
-    DependencyType type;                       // determines what type of graph this object is
-    Integer[][] matrix;                        // holds the value of the dependencies between module
-    String[] fileNameDirectory;                // holds the file name, used to get the index for the matrix
-    Map<String, List<String>> clusters;    // Key is the cluster which the modules belongs to, and value is the list of modules that belong there
-    /* the difference between two versions of DependencyData's, i.e. new nodes */
-    /* only be set when dealing with the 2nd version of the code base */
-    Map<String, List<DependencyEdge>> DependencyDataDifference;
-    
-    public DependencyGraph(DependencyData model) {
-        this.type = model.getType();
-        clusters = new HashMap<String, List<String>>();
-        initGraph(model);
-    }
-
-    private void initGraph(DependencyData model) {
-        int size = model.getSize();
-        matrix = new Integer[size][size];
-
-        for (int i = 0; i < size; i++) {
-
-            for (int j = 0; j < size; j++) {
-                matrix[i][j] = 0;
-            }
-
-        }
-
-        fileNameDirectory = new String[size];
-
-        Map<String, List<DependencyEdge>> map = model.getNodeMap();
-        // keys are all module names in the current "model"(DependencyData) 
-        Set<String> keys = map.keySet();
-        int index = 0;
-        for (String moduleName : keys) {
-            fileNameDirectory[index] = moduleName;
-            index++;
-
-        }
-
-        for (String moduleName : keys) {
-            List<DependencyEdge> edges = map.get(moduleName);
-            int parentIndex = getModuleIndex(moduleName);
-            for (DependencyEdge edge : edges) {
-                int childIndex = getModuleIndex(edge.getChildNode());
-                if (childIndex == -1 || parentIndex == -1) {
-                    System.out.println();
-                }
-                matrix[childIndex][parentIndex] += edge.getNumberOfDependencies();
-                matrix[parentIndex][childIndex] += edge.getNumberOfDependencies();
-            }
-
-        }
+	// determines what type of graph this object is
+	DependencyType type;                       
+	// holds the value of the dependencies between module
+	Integer[][] matrix;                        
+	// holds the file name, used to get the index for the matrix
+	String[] fileNameDirectory;            
+	// Key is the cluster which the modules belongs to, and value is the list of modules that belong there
+	Map<String, List<String>> clusters;    
 
 
-        Map<String, List<DependencyEdge>> dataNodes = model.getNodeMap();
-        for (String key : dataNodes.keySet()) {
-            String clusterName = null;
-            List<DependencyEdge> edges = dataNodes.get(key);
+	/**
+	 * Default constructor
+	 * @param model - DependencyData
+	 */
+	public DependencyGraph(DependencyData model) {
+		this.type = model.getType();
+		clusters = new HashMap<String, List<String>>();
+		initGraph(model);
+	}
 
-            DependencyEdge maxEdge = null;
+	/**
+	 * Initialize the current DependencyGraph instance, by generating values for 
+	 * matrix, fileNameDirectory and clusters
+	 * @param model - DependencyData
+	 */
+	private void initGraph(DependencyData model) {
+		int size = model.getSize();
+		matrix = new Integer[size][size];
 
-            for (DependencyEdge edge : edges) {
+		/* initialize the matrix by filling it with zeros  */
+		for (int i = 0; i < size; i++) {
 
-                if (maxEdge == null || edge.getNumberOfDependencies() > maxEdge.getNumberOfDependencies()) {
+			for (int j = 0; j < size; j++) {
+				matrix[i][j] = 0;
+			}
 
-                    maxEdge = edge;
+		}
 
-                }
-
-            }
-
-            if (maxEdge != null) {
-                clusterName = maxEdge.getChildNode();
-
-            } else {
-                clusterName = key;
-            }
-
-            if (clusters.containsKey(clusterName)) {
-                clusters.get(clusterName).add(key);
-            } else {
-                List<String> value = new ArrayList<String>();
-                value.add(key);
-                clusters.put(clusterName, value);
-            }
-
-        }
+		fileNameDirectory = new String[size];
 
 
-    }
-    
-/*
- * Get the index of the module: name
- * index applies to the above 2-D array 
- */
-    public int getModuleIndex(String name) {
+		/* process the matrix to fill in the number of dependencies for each pair of modules  */
+		Map<String, List<DependencyEdge>> map = model.getNodeMap();
+		// keys are all module names in the current "model"(DependencyData) 
+		Set<String> keys = map.keySet();
+		int index = 0;
+		for (String moduleName : keys) {
+			fileNameDirectory[index] = moduleName;
+			index++;
+
+		}
+
+		for (String moduleName : keys) {
+			List<DependencyEdge> edges = map.get(moduleName);
+			int parentIndex = getModuleIndex(moduleName);
+			for (DependencyEdge edge : edges) {
+				int childIndex = getModuleIndex(edge.getChildNode());
+				if (childIndex == -1 || parentIndex == -1) {
+					System.out.println();
+				}
+				matrix[childIndex][parentIndex] += edge.getNumberOfDependencies();
+				matrix[parentIndex][childIndex] += edge.getNumberOfDependencies();
+			}
+
+		}
 
 
-        for (int index = 0; index < fileNameDirectory.length; index++) {
-            if (name.equals(fileNameDirectory[index])) {
+		/* generate clusters by grouping related modules */
+		Map<String, List<DependencyEdge>> dataNodes = model.getNodeMap();
+		for (String key : dataNodes.keySet()) {
+			String clusterName = null;
+			List<DependencyEdge> edges = dataNodes.get(key);
 
-                return index;
-            }
+			DependencyEdge maxEdge = null;
 
-        }
-        return -1;
+			for (DependencyEdge edge : edges) {
 
-    }
+				if (maxEdge == null || edge.getNumberOfDependencies() > maxEdge.getNumberOfDependencies()) {
+
+					maxEdge = edge;
+
+				}
+
+			}
+
+			if (maxEdge != null) {
+				clusterName = maxEdge.getChildNode();
+
+			} else {
+				clusterName = key;
+			}
+
+			if (clusters.containsKey(clusterName)) {
+				clusters.get(clusterName).add(key);
+			} else {
+				List<String> value = new ArrayList<String>();
+				value.add(key);
+				clusters.put(clusterName, value);
+			}
+
+		}
 
 
-    public Map<String, List<String>> getClusters() {
-        return clusters;
-    }
-    public String[] getFileNameDirectory() {
-        return fileNameDirectory;
-    }
+	}
 
-    public Integer[][] getMatrix() {
-        return matrix;
-    }
+	/**
+	 * Get the index of the module: name
+	 * index applies to the above 2-D array 
+	 */
+	public int getModuleIndex(String name) {
 
-    /**
-     *  generate difference between two DependencyData's, which is a Map<String, List<DependencyEdge>>
-     */
-    private void setDifference(DependencyData model, DependencyData v2Model){
-    	// DependencyDataDifference is a global variable in this class
-    	DependencyDataDifference = new HashMap<String, List<DependencyEdge>>();
-    	Map<String, List<DependencyEdge>> v1NodeMap = model.getNodeMap();
-    	Map<String, List<DependencyEdge>> v2NodeMap = v2Model.getNodeMap();
-    	
-    	//generate newKeys = key difference  between v2NodeMap and the other
-    	Set<String> newKeys = new HashSet<String>(v2NodeMap.keySet());
-    	newKeys.removeAll(v1NodeMap.keySet());
-    	
-    	//generate a Map of list<DependencyEdge>'s for newkeys
-    	for(String key : newKeys){
-    		DependencyDataDifference.put(key, v2NodeMap.get(key));
-    	}
-    	    	
-    	return;
-    }
-    
-    /**
-     * 
-     */
-    public Map<String, List<DependencyEdge>> getDependencyDataDifference(){
-    	try{
-    		return new HashMap<String, List<DependencyEdge>>(DependencyDataDifference);
-    	}catch(NullPointerException ne){
-    		return null;
-    	}
-    }
-    
+
+		for (int index = 0; index < fileNameDirectory.length; index++) {
+			if (name.equals(fileNameDirectory[index])) {
+
+				return index;
+			}
+
+		}
+		return -1;
+
+	}
+
+	/**
+	 * A getter method for the variable clusters
+	 * @return
+	 */
+	public Map<String, List<String>> getClusters() {
+		return clusters;
+	}
+	
+	/**
+	 * A getter method for the variable fileNameDirectory
+	 * @return
+	 */
+	public String[] getFileNameDirectory() {
+		return fileNameDirectory;
+	}
+
+	/**
+	 * A getter method for the variable matrix
+	 * @return
+	 */
+	public Integer[][] getMatrix() {
+		return matrix;
+	}
+
+
 }
